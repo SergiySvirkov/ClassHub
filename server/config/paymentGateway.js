e2137649ff616e22585f663400159f2effd83212
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 13:06:33 +0200

    Add files via upload

---
+++ server/config/paymentGateway.js
@@ -0,0 +1,9 @@
+// Replace the placeholders with your actual payment gateway configuration
+const paymentGatewayConfig = {
+  apiKey: 'your_payment_gateway_api_key',
+  apiSecret: 'your_payment_gateway_api_secret',
+  // Add other necessary configuration options for your payment gateway
+};
+
+module.exports = paymentGatewayConfig;
+
