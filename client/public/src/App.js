3e249e557f4d730a64c72eecd784f8298fbe1b64
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-07 12:59:12 +0200

    Add files via upload

---
+++ client/public/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ClassHub</h1>
+        {/* Add your components and content here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
